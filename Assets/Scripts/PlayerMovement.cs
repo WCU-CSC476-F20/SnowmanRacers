0b501ca1ed105efd9151118b709e255e2c2a2f06
MS832376 <MS832376@wcupa.edu>
2020-12-02 13:06:24 -0500

    Basic Movement
    added basic movement and added setting so that players dont collide with other players

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Vector3 velocity;
+    public float speed = 2f;
+    public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.D)){
+            velocity += transform.right * speed;
+        }
+        if(Input.GetKey(KeyCode.A)){
+            velocity += -transform.right * speed;
+        }
+        if(velocity.x >= 40f){
+            velocity.x = 40f;
+        }else if(velocity.x <= -40f){
+            velocity.x = -40f;
+        }
+        rb.velocity = velocity;
     }
 }
