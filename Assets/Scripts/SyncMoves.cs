b5066360cb68310fb42566e5725b58434f500e89
MS832376 <MS832376@wcupa.edu>
2020-12-02 14:40:56 -0500

    player sync

---
+++ Assets/Scripts/SyncMoves.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class SyncMoves : MonoBehaviour
+{
+    Rigidbody rb;
+    Vector3 latestPos;
+    Quaternion latestRot;
+    Vector3 velocity;
+    Vector3 angularVelocity;
+    PhotonView photonView;
+    bool valuesReceived = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();        
+    }
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            //We own this player: send the others our data
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+            stream.SendNext(rb.velocity);
+            stream.SendNext(rb.angularVelocity);
+        }
+        else
+        {
+            //Network player, receive data
+            latestPos = (Vector3)stream.ReceiveNext();
+            latestRot = (Quaternion)stream.ReceiveNext();
+            velocity = (Vector3)stream.ReceiveNext();
+            angularVelocity = (Vector3)stream.ReceiveNext();
+
+            valuesReceived = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!photonView.IsMine && valuesReceived)
+        {
+            //Update Object position and Rigidbody parameters
+            transform.position = Vector3.Lerp(transform.position, latestPos, Time.deltaTime * 5);
+            transform.rotation = Quaternion.Lerp(transform.rotation, latestRot, Time.deltaTime * 5);
+            rb.velocity = velocity;
+            rb.angularVelocity = angularVelocity;
+        }
+    }
+}
