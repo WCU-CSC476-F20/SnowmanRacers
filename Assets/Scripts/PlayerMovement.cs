b029c8bd31930bbdd86ed108a2bfbd8e9a1526d0
Noah Demi <58009902+NoahDemi@users.noreply.github.com>
2020-12-03 10:27:26 -0500

    Snow theme
    Added:
     Snowman Prefab
     Snow Levels

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,10 @@ using Photon.Pun;
 public class PlayerMovement : MonoBehaviour
 {
     public Vector3 velocity;
-    public float speed = 2f;
+    public float speed = 1f;
+    public float jumpPower;
+    public float jumpHeight = 3f;
+    public bool isJumping = false; 
     public Rigidbody rb;
     PhotonView myView;
     // Start is called before the first frame update
@@ -19,23 +22,18 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(myView == null){
-            myView = GetComponent<PhotonView>();
+        if(Input.GetKey(KeyCode.D)){
+        velocity += transform.right * speed;
         }
-        if(myView.IsMine){
-            if(Input.GetKey(KeyCode.D)){
-            velocity += transform.right * speed;
-            }
-            if(Input.GetKey(KeyCode.A)){
-                velocity += -transform.right * speed;
-            }
-            if(velocity.x >= 40f){
+        if(Input.GetKey(KeyCode.A)){
+            velocity += -transform.right * speed;
+        }
+        if(velocity.x >= 40f){
                 velocity.x = 40f;
-            }else if(velocity.x <= -40f){
+         }else if(velocity.x <= -40f){
                 velocity.x = -40f;
-            }
-            rb.velocity = velocity;
         }
-        
-    }
+        rb.velocity = velocity;
+        }
+           
 }
