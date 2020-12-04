bd783dd27f89a8f1c4b2fcc274eaebb7c624c2a9
mhtf19 <mhtf19@gmail.com>
2020-12-04 13:50:45 -0500

    Update PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,9 +7,11 @@ public class PlayerMovement : MonoBehaviour
 {
     public Vector3 velocity;
     public float speed = 1f;
-    public float jumpPower;
+    public float jumpPower = 8f;
     public float jumpHeight = 3f;
-    public bool isJumping = false; 
+    public float jumpCurrent = 0f;
+    public bool isGrounded = false;
+    public Vector3 jump = new Vector3 (0f, 2f, 0f);
     public Rigidbody rb;
     PhotonView myView;
     // Start is called before the first frame update
@@ -22,18 +24,39 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         if(Input.GetKey(KeyCode.D)){
-        velocity += transform.right * speed;
+            velocity += transform.right * speed;
         }
         if(Input.GetKey(KeyCode.A)){
             velocity += -transform.right * speed;
         }
+        if(Input.GetKey(KeyCode.W)){
+            //transform.position = transform.position + new Vector3(0, 1f * jumpPower * Time.deltaTime, 0);
+            //rb.AddForce(jump*jumpPower*Time.deltaTime, ForceMode.Impulse);
+            //rb.AddForce(Vector3.up*jumpPower);
+            //velocity += jumpPower*transform.up;
+            //print(transform.up);
+            isGrounded = false;
+        }
         if(velocity.x >= 40f){
                 velocity.x = 40f;
-         }else if(velocity.x <= -40f){
+        }else if(velocity.x <= -40f){
                 velocity.x = -40f;
         }
         rb.velocity = velocity;
         }
-           
+/*
+        bool IsGrounded(){
+            return Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.x, col.bounds.min.y, col.bounds.min.y, col.bounds.center.z), col.radius *.8f, groundTags);
+
+        }
+        */
+        void OnCollisionStay()
+        {
+            isGrounded = true;
+        }
+        void OnCollisionExit(){
+             isGrounded = false;
+       }
 }
