dbc520ab76287d6799be541ee6c952a0b97fa302
MS832376 <MS832376@wcupa.edu>
2020-12-08 18:27:33 -0500

    Scroll

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -66,12 +66,6 @@ public class PlayerMovement : MonoBehaviour
                 Renderer tempMat = temp.GetComponent<Renderer>();
                 tempMat.material.SetColor("_Color", Color.green);
              }
-        }else{
-            GameObject[] hat = GameObject.FindGameObjectsWithTag("Hat");
-            foreach(GameObject temp in hat){
-                Renderer tempMat = temp.GetComponent<Renderer>();
-                tempMat.material.SetColor("_Color", Color.red);
-            }
         }
     }
     void FixedUpdate(){
